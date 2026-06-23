afb75aa6658eed749ea089e95137ea581020056e
izer <rezizavkaro@gmail.com>
2026-06-23 23:50:54 +0700

    chore: clear node_modules cache and secure production files

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/greenify/",
   resolve: {
     extensions: [".jsx", ".js", ".ts", ".tsx"],
   },
